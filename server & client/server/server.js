9f8e470d2af5e3ea7e6158e6e1178b2b4d582f85
PabloOch <pabloochoabalderrama251@gmail.com>
2024-02-14 08:24:33 -0800

    update_server_socket

--- server & client/server/server.js
+++ server & client/server/server.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const multer = require('multer')
 const bodyParser = require('body-parser')
-const { WebSocketServer } = require('ws')
 const Dropbox = require('dropbox').Dropbox;
 const fs = require('fs')
 
 const app = express()
 const port = 3000
-const sockserver = new WebSocketServer({ port: 1245 })
-sockserver.on('connection', ws => {
-  console.log('New client connected!')
-  ws.on('close', () => console.log('Client has disconnected!'))
-  ws.on('message', data => {
-   sockserver.clients.forEach(client => {
-     console.log(`distributing message: ${data}`)
-     // client.send(`${data}`)
-   })
-})
-ws.onerror = function () {
-  console.log('websocket error')
-}})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
